3ddb407b609e404322c18e96154440b23027b4f8
Matt Gordon <github@mcgordon.co.uk>
2013-05-16 21:08:04 +0100

    Added MarlinSerial1 Support

--- Marlin/MarlinSerial.h
+++ Marlin/MarlinSerial.h
@@ -21,130 +21,59 @@
 
 #ifndef MarlinSerial_h
 #define MarlinSerial_h
-#include "Marlin.h"
 
+#include <inttypes.h>
 
-#define DEC 10
-#define HEX 16
-#define OCT 8
-#define BIN 2
-#define BYTE 0
+#include "Stream.h"
 
+struct ring_buffer;
 
-#if MOTHERBOARD != 8 // ! teensylu
-// Define constants and variables for buffering incoming serial data.  We're
-// using a ring buffer (I think), in which rx_buffer_head is the index of the
-// location to which to write the next incoming character and rx_buffer_tail
-// is the index of the location from which to read.
-#define RX_BUFFER_SIZE 128
-
-
-struct ring_buffer
+class MarlinSerial : public Stream
 {
-  unsigned char buffer[RX_BUFFER_SIZE];
-  int head;
-  int tail;
+  private:
+    ring_buffer *_rx_buffer;
+    volatile uint8_t *_ubrrh;
+    volatile uint8_t *_ubrrl;
+    volatile uint8_t *_ucsra;
+    volatile uint8_t *_ucsrb;
+    volatile uint8_t *_udr;
+    uint8_t _rxc;
+    uint8_t _rxen;
+    uint8_t _txen;
+    uint8_t _rxcie;
+    uint8_t _udre;
+    uint8_t _u2x;
+  public:
+    MarlinSerial(ring_buffer *rx_buffer,
+      volatile uint8_t *ubrrh, volatile uint8_t *ubrrl,
+      volatile uint8_t *ucsra, volatile uint8_t *ucsrb,
+      volatile uint8_t *udr,
+      uint8_t rxc, uint8_t rxen, uint8_t txen, uint8_t rxcie, uint8_t udre, uint8_t u2x);
+    void begin(long);
+    void end();
+    virtual int available(void);
+    virtual int peek(void);
+    virtual int read(void);
+    virtual void flush(void);
+    virtual void write(uint8_t);
+    using Print::write; // pull in write(str) and write(buf, size) from Print
+    
+    virtual void checkRx(void);
 };
 
 #if defined(UBRRH) || defined(UBRR0H)
-  extern ring_buffer rx_buffer;
+  extern MarlinSerial MSerial;
+#elif defined(USBCON)
+  #include "usb_api.h"
 #endif
-
-class MarlinSerial //: public Stream
-{
-
-  public:
-    MarlinSerial();
-    void begin(long);
-    void end();
-    int peek(void);
-    int read(void);
-    void flush(void);
-    
-    FORCE_INLINE int available(void)
-    {
-      return (unsigned int)(RX_BUFFER_SIZE + rx_buffer.head - rx_buffer.tail) % RX_BUFFER_SIZE;
-    }
-    
-    FORCE_INLINE void write(uint8_t c)
-    {
-      while (!((UCSR0A) & (1 << UDRE0)))
-        ;
-
-      UDR0 = c;
-    }
-    
-    
-    FORCE_INLINE void checkRx(void)
-    {
-      if((UCSR0A & (1<<RXC0)) != 0) {
-        unsigned char c  =  UDR0;
-        int i = (unsigned int)(rx_buffer.head + 1) % RX_BUFFER_SIZE;
-
-        // if we should be storing the received character into the location
-        // just before the tail (meaning that the head would advance to the
-        // current location of the tail), we're about to overflow the buffer
-        // and so we don't write the character or advance the head.
-        if (i != rx_buffer.tail) {
-          rx_buffer.buffer[rx_buffer.head] = c;
-          rx_buffer.head = i;
-        }
-      }
-    }
-    
-    
-    private:
-    void printNumber(unsigned long, uint8_t);
-    void printFloat(double, uint8_t);
-    
-    
-  public:
-    
-    FORCE_INLINE void write(const char *str)
-    {
-      while (*str)
-        write(*str++);
-    }
-
-
-    FORCE_INLINE void write(const uint8_t *buffer, size_t size)
-    {
-      while (size--)
-        write(*buffer++);
-    }
-
-    FORCE_INLINE void print(const String &s)
-    {
-      for (int i = 0; i < (int)s.length(); i++) {
-        write(s[i]);
-      }
-    }
-    
-    FORCE_INLINE void print(const char *str)
-    {
-      write(str);
-    }
-    void print(char, int = BYTE);
-    void print(unsigned char, int = BYTE);
-    void print(int, int = DEC);
-    void print(unsigned int, int = DEC);
-    void print(long, int = DEC);
-    void print(unsigned long, int = DEC);
-    void print(double, int = 2);
-
-    void println(const String &s);
-    void println(const char[]);
-    void println(char, int = BYTE);
-    void println(unsigned char, int = BYTE);
-    void println(int, int = DEC);
-    void println(unsigned int, int = DEC);
-    void println(long, int = DEC);
-    void println(unsigned long, int = DEC);
-    void println(double, int = 2);
-    void println(void);
-};
-
-extern MarlinSerial MSerial;
-#endif // ! teensylu
+#if defined(UBRR1H)
+  extern MarlinSerial MSerial1;
+#endif
+#if defined(UBRR2H)
+  extern MarlinSerial MSerial2;
+#endif
+#if defined(UBRR3H)
+  extern MarlinSerial MSerial3;
+#endif
 
 #endif
