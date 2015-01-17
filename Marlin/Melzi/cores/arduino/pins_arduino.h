49faf5d7083e2df159223caa3e03cbd1ed077a00
twistedsanity <twistedsanity@users.noreply.github.com>
2015-01-17 21:09:13 +0000

    Tweaks to Melzi bsp to work with ino
    Tweaks to Melzi bsp to work with ino

--- Marlin/Melzi/cores/arduino/pins_arduino.h
+++ Marlin/Melzi/cores/arduino/pins_arduino.h
@@ -1,24 +1,19 @@
 /*
   pins_arduino.h - Pin definition functions for Arduino
   Part of Arduino - http://www.arduino.cc/
-
   Copyright (c) 2007 David A. Mellis
-
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
-
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
-
   You should have received a copy of the GNU Lesser General
   Public License along with this library; if not, write to the
   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
   Boston, MA  02111-1307  USA
-
   $Id: wiring.h 249 2007-02-03 16:52:51Z mellis $
 */
 
@@ -101,5 +96,8 @@ extern const uint8_t PROGMEM digital_pin_to_timer_PGM[];
 #define portOutputRegister(P) ( (volatile uint8_t *)( pgm_read_word( port_to_output_PGM + (P))) )
 #define portInputRegister(P) ( (volatile uint8_t *)( pgm_read_word( port_to_input_PGM + (P))) )
 #define portModeRegister(P) ( (volatile uint8_t *)( pgm_read_word( port_to_mode_PGM + (P))) )
+#define NUM_DIGITAL_PINS            32
+#define NUM_ANALOG_INPUTS           8
+#define analogInputToDigitalPin(p)  ((p < NUM_ANALOG_INPUTS) ? (p) + NUM_DIGITAL_PINS - NUM_ANALOG_INPUTS : -1)
 
 #endif
